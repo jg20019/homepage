5640a306fb00fed791184f3f39b89c9770613361
John Gibson <jg20019@gmail.com>
2023-04-08 16:49:53 -0400

    feat: Add background component

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,10 @@
 <script>
 import Clock from './lib/Clock.svelte'
+import Background from './lib/Background.svelte';
 </script>
 
 <main>
+<Background />
 <Clock />
 </main>
 
---
+++ src/lib/Background.svelte
@@ -0,0 +1,21 @@
+<script>
+
+let src = 'images/jakob-owens-uWbRcJSJLV8-unsplash.jpg'
+$: style = `background-image: url("${src}");`
+
+</script>
+
+<div {style} ></div>
+
+<style>
+div {
+    z-index: -999;
+    background-color: red;
+    background-size: cover;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+}
+</style>
