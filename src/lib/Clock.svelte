c1a056327400f7c0c4600952d1e4b221a2a1885f
John Gibson <jg20019@gmail.com>
2023-04-08 16:29:28 -0400

    feat: Create clock component

---
+++ src/lib/Clock.svelte
@@ -0,0 +1,34 @@
+<script>
+  import { onMount } from "svelte";
+
+  let time = new Date()
+
+  $: hours = zeroPad(time.getHours())
+  $: minutes = zeroPad(time.getMinutes())
+  $: seconds = zeroPad(time.getSeconds())
+
+
+  onMount(() => {
+      const interval = setInterval(() => {
+          time = new Date()
+      }, 1000)
+
+      return () => {
+          clearInterval(interval)
+      }
+  })
+
+  function zeroPad(n) {
+      return `${n}`.padStart(2, '0') 
+  }
+</script>
+
+<div>
+  {hours}:{minutes}:{seconds}
+</div>
+
+<style>
+ div {
+     font-size: 4em;
+ }
+</style>
